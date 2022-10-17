d4cb18a19c2198897c500ad04f2f63bb3212e33f
Archit Dadwal <83974549+architdadwal@users.noreply.github.com>
2022-10-17 13:01:13 +0530

    added more

--- sumZeroNew.js
+++ sumZeroNew.js
@@ -20,3 +20,24 @@ function findSumPair(array) {
 }
 const final = findSumPair([-5, -4, -3, -2, 0, 2, 4, 6, 8]);
 console.log(final);
+
+//trying with a diffeent array
+//array = [-3,-2,-1,0,2,3]
+
+function findPair(array) {
+  let left = 0;
+  let right = array.length - 1;
+  while (left < right) {
+    sum = array[left] + array[right];
+    if (sum === 0) {
+      return [array[left], array[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+const add = findPair([-3, -2, -1, 0, 2, 3]);
+console.log(add);
